30cedde4f4b93de7e748e30489dc691851c717e8
xuanviet73 <2012393@dlu.edu.vn>
2023-07-03 00:52:46 +0700

    VR

---
+++ data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-qung-trng-lm-vin",
+      "name": "Quảng trường Lâm Viên",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.688896683520813,
+          "pitch": 0.2620501643389872,
+          "rotation": 0,
+          "target": "1-kfe-house-hoa-atiso"
+        },
+        {
+          "yaw": -0.2582157650732775,
+          "pitch": 0.41594455956191645,
+          "rotation": 0,
+          "target": "2-nh-ht-opera-hoa-d-qu"
+        },
+        {
+          "yaw": -0.6786147722356617,
+          "pitch": 0.16410374942742934,
+          "rotation": 9.42477796076938,
+          "target": "3-cinestar--lt"
+        },
+        {
+          "yaw": 1.0798589375761,
+          "pitch": 0.15181788949384512,
+          "rotation": 9.42477796076938,
+          "target": "4-go-da-lat"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kfe-house-hoa-atiso",
+      "name": "K'fe House (hoa atiso)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1801619067427076,
+          "pitch": 0.004448977183418634,
+          "rotation": 0,
+          "target": "0-qung-trng-lm-vin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-nh-ht-opera-hoa-d-qu",
+      "name": "Nhà hát Opera hoa dã quỳ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.020831852817075003,
+          "pitch": -0.20577867205461153,
+          "rotation": 0,
+          "target": "0-qung-trng-lm-vin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cinestar--lt",
+      "name": "Cinestar Đà Lạt",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9471435501574739,
+          "pitch": -0.08855706893043447,
+          "rotation": 1.5707963267948966,
+          "target": "0-qung-trng-lm-vin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-go-da-lat",
+      "name": "GO! Da Lat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.49043526757042955,
+          "pitch": -0.3730745456553777,
+          "rotation": 0.7853981633974483,
+          "target": "0-qung-trng-lm-vin"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
